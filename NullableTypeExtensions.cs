b9b079a8ef400cc08bfcebf67b385d4a8436e12d
Dima Naumow <dimanaumow123@gmail.com>
2019-02-26 01:21:47 +0300

    first attempt

--- NullableTypeExtensions.cs
+++ NullableTypeExtensions.cs
@@ -6,8 +6,12 @@ namespace PadawansTask10
     {
         public static bool IsNull(this object variable)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if(variable == null)
+            {
+                return true; 
+            }
+
+            return false; 
         }
     }
 }
